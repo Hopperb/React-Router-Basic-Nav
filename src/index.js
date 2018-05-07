72888a5983e2fe76f7f199d7f3e9bae67d48a3ce
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-07 12:49:00 -0400

    -ran yarn install and implemented React Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+
+
+ReactDOM.render(
+    <Router>
+      <App />
+    </Router>,
+    document.getElementById("root")
+  );
 
 ReactDOM.render(<App />, document.getElementById('root'));
